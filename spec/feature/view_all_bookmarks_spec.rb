a0de64b2b0518c84cfe1ef4fb8fa181e09384eac
ian <kickthechair@gmail.com>
2022-02-21 17:15:27 +0000

    adjusted feature test syntax

--- spec/feature/view_all_bookmarks_spec.rb
+++ spec/feature/view_all_bookmarks_spec.rb
@@ -4,11 +4,11 @@ feature 'view bookmarks' do
     visit('/bookmarks')
     expect(page).to have_content('Bookmark Manager')
     expect(page).to have_content('www.google.com')
-    page.find(:css, 'a[href="http://www.google.com"]')
+    expect(page).to have_link(href: "http://www.google.com")
     expect(page).to have_content('www.bbc.com')
-    page.find(:css, 'a[href="http://www.bbc.com"]')
+    expect(page).to have_link(href: "http://www.bbc.com")
     expect(page).to have_content('www.youtube.com')
-    page.find(:css, 'a[href="http://www.youtube.com"]')
+    expect(page).to have_link(href: "http://www.youtube.com")
   end
 
 end
