4b43d98ce677d282360d998b7d4fffe2fb40ef0f
ian <kickthechair@gmail.com>
2022-03-02 18:32:16 +0000

    Adjusted feature test so that a button is required to get to the '/bookmarks/new' route. Also inserted a new form into the HTML for said button to 'Add Bookmark'

--- spec/feature/add_new_bookmark_spec.rb
+++ spec/feature/add_new_bookmark_spec.rb
@@ -1,10 +1,10 @@
 feature 'add new bookmark' do
 
   scenario 'add a new bookmark and display it in the complete list' do
-    visit('/bookmarks/new')
+    visit('/bookmarks')
+    click_button("Add Bookmark")
     fill_in('Add New Bookmark', with: "https://www.reuters.com/")
     click_button("Submit")
-
     expect(page).to have_content("https://www.reuters.com/")
   end
 
